b10660fe43e170286d0e9d7f7b4002fcf727a3b9
teresa <143844055+Teresa2006@users.noreply.github.com>
2023-09-09 20:04:16 +0000

    avance reatc y html

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -10,13 +10,15 @@ export default function Home() {
 
       <a href="/flex"> Go to Flex </a>
       <a href="/styled"> Go to Styled </a>
+            <a href="/styled"> Go to Styled </a>
 
-      <p>Home</p>
+
+      <p className="Home">Home</p>
       
       <div className="box" > Box </div>
-
+      <div className="box2" > Box </div>
       <img
-        src={'https://imagizer.imageshack.com/img538/9889/xlMaV6.jpg'}
+        src={'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/cb402ebfc70f6cb272804ae0d485b55d~c5_720x720.jpeg?x-expires=1694383200&x-signature=LLahhHoz5mjisoUAV7FLTvifxsY%3D'}
         className="imageclass"
       /> 
 
